93e5945b4779c00114b52812e571d0230fca7875
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2024-04-03 20:53:57 +0900

    변수타입 변경

--- tf_scenario_code/1_pre-condition-ec2/vars.tf
+++ tf_scenario_code/1_pre-condition-ec2/vars.tf
@@ -13,14 +13,14 @@ variable ec2_type {
 }
 
 variable ami_id {
-  type = string
+  type = list(string)
   # default = "ami-0c1f7b7eb05c17ca5"
   default = "ami-0c031a79ffb01a803"
   description = "Amazon Linux 2023 AMI ARM64 지원 AMI"
 }
 
 variable ami_name_filter {
-  type = list(string)
+  type = string
   default = "*al2023*-arm64"
   # default = "*al2023*-x86_64"
   description = "Amazon Linux 2023 AMI ARM64 지원 AMI"
