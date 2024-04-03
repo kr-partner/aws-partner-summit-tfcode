8e01949108502fc422334ca4b7d9ee89043ccb1e
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2024-04-03 20:54:34 +0900

    변수타입 ["string 값"]

--- tf_scenario_code/1_pre-condition-ec2/vars.tf
+++ tf_scenario_code/1_pre-condition-ec2/vars.tf
@@ -15,7 +15,7 @@ variable ec2_type {
 variable ami_id {
   type = list(string)
   # default = "ami-0c1f7b7eb05c17ca5"
-  default = "ami-0c031a79ffb01a803"
+  default = ["ami-0c031a79ffb01a803"]
   description = "Amazon Linux 2023 AMI ARM64 지원 AMI"
 }
 
