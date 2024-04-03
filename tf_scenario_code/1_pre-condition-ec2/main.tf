743d228e606a7fb7265ee83eff8453ef90545dea
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2024-04-03 20:45:04 +0900

    aws_ami updated

--- tf_scenario_code/1_pre-condition-ec2/main.tf
+++ tf_scenario_code/1_pre-condition-ec2/main.tf
@@ -18,18 +18,13 @@
 data "aws_ami" "al2023_arm" {
   most_recent = true
 
-  owners = ["amazon"]
+  # owners = ["amazon"]
   
   filter {
     name = "image-id"
     values = [var.ami_id]
     # ami-0c031a79ffb01a803는 x86_64 이미지
     # ami-0c1f7b7eb05c17ca5는 arm64 이미지
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
   }
 }
 
