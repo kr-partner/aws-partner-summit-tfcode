e51f0f02649d5ef959e62405b254eb62adc6b35f
jangwhan <84706625+awswa@users.noreply.github.com>
2024-04-07 12:21:36 +1000

    Update main.tf
    acl_enabled를 acl_disabled로 변경

--- tf_scenario_code/3_sentinel-s3/main.tf
+++ tf_scenario_code/3_sentinel-s3/main.tf
@@ -17,14 +17,14 @@ resource "aws_s3_bucket_acl" "s3-bucket-acl" {
   depends_on = [aws_s3_bucket_ownership_controls.s3-bucket-ownership]
 
   bucket = aws_s3_bucket.s3-bucket-sentinel.id
-  acl    = var.acl_enabled ? "private" : "public-read"
+  acl    = var.acl_disabled ? "private" : "public-read"
 }
 
-resource "aws_s3_bucket_public_access_block" "s3-bucket-public-access" {
-  bucket = aws_s3_bucket.s3-bucket-sentinel.id
+#resource "aws_s3_bucket_public_access_block" "s3-bucket-public-access" {
+#  bucket = aws_s3_bucket.s3-bucket-sentinel.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#  block_public_acls       = false
+#  block_public_policy     = false
+#  ignore_public_acls      = false
+#  restrict_public_buckets = false
+#}
