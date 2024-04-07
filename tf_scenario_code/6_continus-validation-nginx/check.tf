4a0fe47468796fe818a1701417e0632397cb3fc1
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2024-04-07 17:28:41 +0900

    Check Block 테스트

--- tf_scenario_code/6_continus-validation-nginx/check.tf
+++ tf_scenario_code/6_continus-validation-nginx/check.tf
@@ -2,8 +2,8 @@ check "response_nginx" {
   data "http" "nginx" {
    url      = "http://${aws_instance.ec2.public_dns}"
 
-  # depends_on = [time_sleep.wait_15_seconds]
-  depends_on = [check.response_ec2_running]
+  depends_on = [time_sleep.wait_15_seconds]
+  # depends_on = [check.response_ec2_running]
 }
 
   assert {
@@ -14,7 +14,6 @@ check "response_nginx" {
 
 check "response_ec2_running" {
 
-  # depends_on = [time_sleep.wait_15_seconds]
   assert {
     condition     = aws_instance.ec2.instance_state == "running"
     error_message = "EC2 인스턴스가 Running 상태가 아닙니다."
